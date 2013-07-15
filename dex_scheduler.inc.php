b7d9e5421329264b39fdba8f2d932c5fadc0fbdc
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-15 18:01:01 +0000

    Some third party plugins are inserting BR and P tags between input tags and empty lines in the public website when using a shortcode to replace content. Some modifications have been made to remove the line breaks.
    git-svn-id: https://plugins.svn.wordpress.org/cp-appointment-calendar/trunk@741215 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dex_scheduler.inc.php
+++ dex_scheduler.inc.php
@@ -23,13 +23,9 @@ var dex_global_maxdate = '<?php $value = get_option('calendar_maxdate', DEX_APPO
 <script type="text/javascript" src="<?php echo plugins_url('TDE_AppCalendar/all-scripts.js', __FILE__); ?>"></script>
 <script>
 initAppCalendar("cal1",<?php echo get_option('calendar_pages', DEX_APPOINTMENTS_DEFAULT_CALENDAR_PAGES); ?>,2,"<?php echo get_option('calendar_language', DEX_APPOINTMENTS_DEFAULT_CALENDAR_LANGUAGE); ?>",{m1:"<?php _e('Please select the appointment time.'); ?>"});
-</script>
-<input name="dex_appointments_post" type="hidden" id="1" /><input name="selDaycal1" type="hidden" id="selDaycal1" /><input name="selMonthcal1" type="hidden" id="selMonthcal1" /><input name="selYearcal1" type="hidden" id="selYearcal1" /><input name="selHourcal1" type="hidden" id="selHourcal1" /><input name="selMinutecal1" type="hidden" id="selMinutecal1" />
+</script><input name="dex_appointments_post" type="hidden" id="1" /><input name="selDaycal1" type="hidden" id="selDaycal1" /><input name="selMonthcal1" type="hidden" id="selMonthcal1" /><input name="selYearcal1" type="hidden" id="selYearcal1" /><input name="selHourcal1" type="hidden" id="selHourcal1" /><input name="selMinutecal1" type="hidden" id="selMinutecal1" />
 <div class="appContainer">
-<div style="z-index:1000;" class="appContainer2">
-  <div id="cal1Container"></div>
-</div>
-</div>
+<div style="z-index:1000;" class="appContainer2"><div id="cal1Container"></div></div></div>
 <div style="clear:both;"></div>
 <div id="selddiv" style="font-weight: bold;margin-top:5px;padding:3px;"></div>
 <script type="text/javascript">
@@ -95,20 +91,14 @@ initAppCalendar("cal1",<?php echo get_option('calendar_pages', DEX_APPOINTMENTS_
     return true;
  } 
 </script>
-
 <br />
-
 <?php _e('Your phone number'); ?>:<br />
 <input type="text" name="phone" value=""><br />
-
 <?php _e('Your name'); ?>:<br />
 <input type="text" name="name" value=""><br />
-
 <?php _e('Your email'); ?>:<br />
 <input type="text" name="email" value=""><br />
-
 <?php _e('Comments/Questions'); ?>:<br />
 <textarea name="question" style="width:100%"></textarea><br />
-
 <input type="submit" name="subbtn" value="<?php _e("Continue"); ?>">
 </form>
